036136ec5e312a7940836228f5c056d639bbf297
justin-csiky <justin.csiky@gmail.com>
2026-04-23 00:34:54 +0200

    More hiding

--- Main.py
+++ Main.py
@@ -13,6 +13,11 @@ st.markdown("""
         top: 0;
         height: 100vh;
     }
+    [data-testid="stToolbar"] {
+        visibility: hidden;
+        height: 0%;
+        position: fixed;
+    }
 </style>
 """, unsafe_allow_html=True)
 # --- DATABASE SETUP ---
