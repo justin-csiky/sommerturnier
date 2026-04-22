df65b333ec92acaf4adc711bb49e332eeb53ee7f
justin-csiky <justin.csiky@gmail.com>
2026-04-23 01:04:42 +0200

    Footer hidden, surely last

--- Main.py
+++ Main.py
@@ -8,6 +8,7 @@ st.markdown("""
     [data-testid="stMainMenu"] {display: none;}
     [data-testid="stToolbarActions"] {display: none;}
     [data-testid="appCreatorAvatar"] {display: none;}
+    footer {visibility: hidden;}
 </style>
 """, unsafe_allow_html=True)
 # --- DATABASE SETUP ---
